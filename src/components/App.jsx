c86a0515562a6f7a223f0d214139a48ec65a875d
Ben Fischer <fischerben010@gmail.com>
2021-12-01 14:40:19 -0600

    Added some components that will be needed for the future

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from "react-router-dom";
+import jwtDecode from 'jwt-decode';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedInUser: null,
+        }
+    }
+    render() { 
+        return (
+            <div>
+                <h1>Hello World</h1>
+
+                <div>
+                    
+                </div>
+
+            </div>
+        );
+    }
+}
+ 
+export default App;
